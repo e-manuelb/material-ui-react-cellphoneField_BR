77414b167624e9023c9666acb8de85f39790e3f5
Emanuel C. Barros <50119229+e-manuelb@users.noreply.github.com>
2021-09-10 08:41:00 -0300

    a little update, just to make smaller

--- TextField.jsx
+++ TextField.jsx
@@ -8,104 +8,70 @@ import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        "& > *": {
-            margin: theme.spacing(1)
-        }
+  root: {
+    "& > *": {
+      margin: theme.spacing(1)
     }
+  }
 }));
 
 function TextMaskCustom(props) {
-    const { inputRef, ...other } = props;
+  const { inputRef, ...other } = props;
 
-    return (
-        <MaskedInput
-            {...other}
-            ref={(ref) => {
-                inputRef(ref ? ref.inputElement : null);
-            }}
-            mask={[
-                "(",
-                /[1-9]/,
-                /\d/,
-                ")",
-                " ",
-                /\d/,
-                /\d/,
-                /\d/,
-                /\d/,
-                /\d/,
-                "-",
-                /\d/,
-                /\d/,
-                /\d/,
-                /\d/
-            ]}
-            placeholderChar={"\u2000"}
-            showMask
-        />
-    );
+  return (
+    <MaskedInput
+      {...other}
+      ref={(ref) => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={[
+        "(",
+        /[1-9]/,
+        /\d/,
+        ")",
+        " ",
+        /\d/,
+        /\d/,
+        /\d/,
+        /\d/,
+        /\d/,
+        "-",
+        /\d/,
+        /\d/,
+        /\d/,
+        /\d/
+      ]}
+      placeholderChar={"\u2000"}
+      showMask
+    />
+  );
 }
-
-TextMaskCustom.propTypes = {
-    inputRef: PropTypes.func.isRequired
-};
-
-function NumberFormatCustom(props) {
-    const { inputRef, onChange, ...other } = props;
-
-    return (
-        <NumberFormat
-            {...other}
-            getInputRef={inputRef}
-            onValueChange={(values) => {
-                onChange({
-                    target: {
-                        name: props.name,
-                        value: values.value
-                    }
-                });
-            }}
-            thousandSeparator
-            isNumericString
-            prefix="$"
-        />
-    );
-}
-
-NumberFormatCustom.propTypes = {
-    inputRef: PropTypes.func.isRequired,
-    name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
-};
 
 export default function FormattedInputs() {
-    const classes = useStyles();
-    const [values, setValues] = React.useState({
-        textmask: "",
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    textmask: ""
+  });
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
     });
+  };
 
-    const handleChange = (event) => {
-        setValues({
-            ...values,
-            [event.target.name]: event.target.value
-        });
-    };
-
-    return (
-        <div className={classes.root}>
-            <FormControl>
-                <InputLabel htmlFor="formatted-text-mask-input">
-                    Telefone
-                </InputLabel>
-                <Input
-                    value={values.textmask}
-                    onChange={handleChange}
-                    name="textmask"
-                    id="formatted-text-mask-input"
-                    inputComponent={TextMaskCustom}
-                />
-            </FormControl>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <FormControl>
+        <InputLabel htmlFor="formatted-text-mask-input">Telefone</InputLabel>
+        <Input
+          value={values.textmask}
+          onChange={handleChange}
+          name="textmask"
+          id="formatted-text-mask-input"
+          inputComponent={TextMaskCustom}
+        />
+      </FormControl>
+    </div>
+  );
 }
